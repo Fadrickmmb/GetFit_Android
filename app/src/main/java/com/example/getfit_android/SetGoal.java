b4711fcb415599f4f752b880748fe69df0e20aab
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-07-04 13:54:22 -0400

    Starting to work on the NavBar for activity_set_goal.xml; Creation of Profile.java and activity_profile.xml;

--- app/src/main/java/com/example/getfit_android/SetGoal.java
+++ app/src/main/java/com/example/getfit_android/SetGoal.java
@@ -1,6 +1,10 @@
 package com.example.getfit_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class SetGoal extends AppCompatActivity {
+
+
+    TextView username;
+    LinearLayout profile, today, history, meal;
+    FloatingActionButton setGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,37 @@ public class SetGoal extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        profile = findViewById(R.id.profileNavButton);
+        today = findViewById(R.id.todayNavButton);
+        history = findViewById(R.id.historyNavButton);
+        meal = findViewById(R.id.mealNavButton);
+        setGoal = findViewById(R.id.setGoalButton);
+        username = findViewById(R.id.setGoalUsername);
+
+
+        today.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Today.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        meal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MealCalories.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+
+
     }
 }
