3d50c6edc1d4a0da0a015b5b2006ae2ddb6cd9dd
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-08-03 21:02:38 -0400

    Started working on History.java and activity_history.xml

--- app/src/main/java/com/example/getfit_android/History.java
+++ app/src/main/java/com/example/getfit_android/History.java
@@ -1,6 +1,9 @@
 package com.example.getfit_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class History extends AppCompatActivity {
+
+    LinearLayout toProfile, toHistory, toMainScreen, toMealCalories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,50 @@ public class History extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        toProfile = findViewById(R.id.historyProfileNavButton);
+        toHistory = findViewById(R.id.historyHistoryNavButton);
+        toMainScreen = findViewById(R.id.historyTodayNavButton);
+        toMealCalories = findViewById(R.id.historyMealNavButton);
+
+
+        toProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Profile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        toHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), History.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        toMainScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        toMealCalories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MealCalories.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
 }
