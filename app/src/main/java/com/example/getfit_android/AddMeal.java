6853bc0f85e44551dbde4db77c206bab7e586ff3
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-08-13 10:06:28 -0400

    Working on AddMeal.java

--- app/src/main/java/com/example/getfit_android/AddMeal.java
+++ app/src/main/java/com/example/getfit_android/AddMeal.java
@@ -1,6 +1,12 @@
 package com.example.getfit_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddMeal extends AppCompatActivity {
+
+    EditText mealName, mealDescription;
+
+    Button addMeal, backToToday;
+
+    TextView apiResponse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,40 @@ public class AddMeal extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        backToToday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        addMeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameOfMeal = mealName.getText().toString().trim();
+                String descriptionOfMeal = mealDescription.getText().toString().trim();
+
+                if (nameOfMeal.isEmpty() || descriptionOfMeal.isEmpty()){
+                    Toast.makeText(AddMeal.this, "Please fill both Inputs", Toast.LENGTH_SHORT).show();
+                }else{
+                    addMealToTextView(nameOfMeal, descriptionOfMeal);
+                    addMealToDB(nameOfMeal, descriptionOfMeal);
+                }
+
+            }
+        });
+
     }
+
+    private void addMealToTextView(String name, String description){
+
+    }
+
+    private void addMealToDB(String name, String description){
+
+    }
+
 }
