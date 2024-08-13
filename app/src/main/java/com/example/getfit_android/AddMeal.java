b46d3aa293233de17afc005c75533cf2d289e128
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-08-13 10:57:06 -0400

    Working on the Display of the Meal for the AddMeal.java

--- app/src/main/java/com/example/getfit_android/AddMeal.java
+++ app/src/main/java/com/example/getfit_android/AddMeal.java
@@ -14,13 +14,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddMeal extends AppCompatActivity {
 
     EditText mealName, mealDescription;
 
     Button addMeal, backToToday;
-
     TextView apiResponse;
+
+    RequestQueue requestQueue;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
 
     @Override
@@ -33,6 +47,16 @@ public class AddMeal extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        requestQueue = Volley.newRequestQueue(this);
+        backToToday = findViewById(R.id.addMealBackToMain);
+        mealName = findViewById(R.id.addMealmealName);
+        mealDescription = findViewById(R.id.addMealmealDescription);
+        apiResponse = findViewById(R.id.addMealTextView);
+        addMeal = findViewById(R.id.addMealAddButton);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference("users");
 
         backToToday.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +86,33 @@ public class AddMeal extends AppCompatActivity {
     }
 
     private void addMealToTextView(String name, String description){
+
+        String url = "https://api.calorieninjas.com/v1/nutrition?query=" + description;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        parseAndDisplayResponse(response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(AddMeal.this, "Failed to fetch data", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            public java.util.Map<String, String> getHeaders() {
+                java.util.Map<String, String> params = new java.util.HashMap<>();
+                params.put("X-Api-Key", "6VVStmeL3WusRAYKusjoAw==8lz8IfitH9UU7t5s");
+                return params;
+            }
+        };
+
+    }
+
+    private void parseAndDisplayResponse(String response){
 
     }
 
